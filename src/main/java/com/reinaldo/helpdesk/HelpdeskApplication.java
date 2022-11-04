2e62392269c398468cb3ba57bfeb2537a33047f3
Reinaldo44 <reinaldoap82@hotmail.com>
2022-11-04 11:26:31 -0300

    desenvolvimento perfil de dev

--- src/main/java/com/reinaldo/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/reinaldo/helpdesk/HelpdeskApplication.java
@@ -1,11 +1,9 @@
 package com.reinaldo.helpdesk;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelpdeskApplication{
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
